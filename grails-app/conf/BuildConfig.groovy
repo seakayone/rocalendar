6ba91557a44eec8e2920c5b7c0c9f4b2a664c59f
Christian Kleinbölting <christian@rootsouttacontrol.de>
2012-04-24 15:25:47 +0200

    Fixed issue with missing plugin webxml

--- grails-app/conf/BuildConfig.groovy
+++ grails-app/conf/BuildConfig.groovy
@@ -42,6 +42,7 @@ grails.project.dependency.resolution = {
         runtime ":jquery:1.7.1"
         runtime ":resources:1.1.6"
 
+	compile 'webxml:webxml:1.4.1'
         // Uncomment these (or add new ones) to enable additional resources capabilities
         //runtime ":zipped-resources:1.0"
         //runtime ":cached-resources:1.0"
