c163772d25302f898a412ce6c31f6d97123f687e
Christian Kleinbölting <christian@rootsouttacontrol.de>
2012-04-25 14:39:45 +0200

    added sample venues to bootstrap, as well as two sample users

--- grails-app/conf/BootStrap.groovy
+++ grails-app/conf/BootStrap.groovy
@@ -1,22 +1,23 @@
 import de.rootsouttacontrol.rocalendar.SecRole
 import de.rootsouttacontrol.rocalendar.SecUser
 import de.rootsouttacontrol.rocalendar.SecUserSecRole
-
+import de.rootsouttacontrol.rocalendar.Venue
 
 class BootStrap {
 
 	def init = { servletContext ->
+		
+		def venue1 = new Venue(name: 'Baobab', street: 'Wenckstr. 1', zip: '64289', city:'Darmstadt', state:'Hessen', country:'Germany', url: 'http://baobab-erlebnisbar.de', description: 'description', lat: '49.8798200', lng: '8.6590900').save()
+		def venue2 = new Venue(name: '603qm', street: 'Alexanderstr. 2', zip: '64289', city:'Darmstadt', state:'Hessen', country:'Germany', url: 'http://603qm.de', description: 'description', lat: '49.8749700', lng: '8.6575700').save()
+		
 		def adminRole = new SecRole(authority: 'ROLE_ADMIN').save(flush: true)
-		
 		def userRole = new SecRole(authority: 'ROLE_USER').save(flush: true)
-
-		def testUser = new SecUser(username: 'me', enabled: true, password: 'password').save(flush: true)
 		
-		SecUserSecRole.create(testUser, adminRole, true)
-
-		assert SecUser.count() == 1
-		assert SecRole.count() == 2
-		assert SecUserSecRole.count() == 1
+		def testUser = new SecUser(username: 'meUser', enabled: true, password: 'password').save(flush: true)
+		SecUserSecRole.create(testUser, userRole, true)
+		
+		def testAdmin = new SecUser(username: 'meAdmin', enabled: true, password: 'password').save(flush: true)
+		SecUserSecRole.create(testAdmin, adminRole, true)
 	}
 	def destroy = {
 	}
