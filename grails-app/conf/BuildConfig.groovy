e2987b288ff7ab985b6c560c420dcf8f1dab652a
Christian Kleinbölting <christian@rootsouttacontrol.de>
2012-04-30 17:36:45 +0200

    removed dbmigration plugin from master, as deployment on heroku is not yet working

--- grails-app/conf/BuildConfig.groovy
+++ grails-app/conf/BuildConfig.groovy
@@ -41,7 +41,7 @@ grails.project.dependency.resolution = {
         runtime ":jquery:1.7.1"
         runtime ":resources:1.1.6"
 
-	compile ":database-migration:1.0"
+	// compile ":database-migration:1.0"
 	compile "webxml:webxml:1.4.1"
         // Uncomment these (or add new ones) to enable additional resources capabilities
         //runtime ":zipped-resources:1.0"
